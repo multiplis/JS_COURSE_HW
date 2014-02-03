698a1147f0835bdd91e6e3cc728a88484094ba55
unknown <y.mazurenko@ymazurenko.kiev.astoundcommerce.com>
2014-02-03 15:57:33 +0200

    adds keypress events

--- hw_6/2/script.js
+++ hw_6/2/script.js
@@ -2,8 +2,7 @@ function selectItem(item){
   var select = document.getElementsByClassName("selected");
   Array.prototype.forEach.call(select, function(cell){
     cell.className = "";
-  });
-  
+  });  
   item.target.className = "selected";
 }
 
@@ -11,4 +10,52 @@ function setBindings(){
   var table = document.getElementById("field");
   table.addEventListener("click", selectItem,  false);
 }
+function setKeyboardEvents(){
+  document.onkeypress = function(e){
+  
+    switch (e.keyCode){
+      case 37:
+        moveLeft();
+        break;
+      case 38:
+        moveUp();
+        break;
+      case 39:
+        moveRight();
+        break;
+      case 40:
+        moveDown();
+        break;
+      case 13:
+        if(e.shiftKey){
+          addColumn();
+        }else{
+          addRow();
+        }
+        break;
+      default:
+        console.log("default", e.keyCode);
+        break;
+    }
+  };
+}
+function moveLeft(){
+  console.log('left');
+}
+function moveUp(){
+  console.log('up');
+}
+function moveRight(){
+  console.log('right');
+}
+function moveDown(){
+  console.log('down');
+}
+function addColumn(){
+  console.log("add column");
+}
+function addRow(){
+  console.log("add row");
+}
+setKeyboardEvents();
 setBindings();
